510ba07e5cf9e00f7f8e11a95e545edb87232c8a
Andre J <andre.jankovsky@scotiabank.com>
2016-09-09 14:52:29 -0400

    Missed file recovered

---
+++ src/test/java/com/example/SpringLoggingHelper.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+ 
+public class SpringLoggingHelper {
+    
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+   
+    public void helpMethod(){
+        logger.debug("This is a debug message from helper in MicroService-01");
+        logger.info("This is an info message from helper in MicroService-01");
+        logger.warn("This is a warn message from helper in MicroService-01");
+        logger.error("This is an error message from helper in MicroService-01");
+ 
+    }
+}
